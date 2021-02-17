1cc555b718f921128b7d598d93e682b3396f729e
MohamedBilelBesbes <bilel.besbes1@gmail.com>
2021-02-17 22:02:55 +0100

    el angular mte3 lavant gout mte3 ZC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { AccountDetailsComponent } from './account-details/account-details.component'; import { CreateAccountComponent } from './create-account/create-account.component'; import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { accountListComponent } from './account-list/account-list.component';
+import { UpdateAccountComponent } from './update-account/update-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', redirectTo: 'account', pathMatch: 'full' },
+{ path: 'accounts', component: accountListComponent },
+{ path: 'add', component: CreateAccountComponent },
+{ path: 'update/:id', component: UpdateAccountComponent },
+{ path: 'details/:id', component: AccountDetailsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+ 
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
-
-@Component({
+import { Component } from '@angular/core'; @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'angular8-springboot-client';
-}
+  templateUrl: './app.component.html', styleUrls: ['./app.component.css']
+  })
+  export class AppComponent { title = 'Accounts Manager';
+  }
+  
